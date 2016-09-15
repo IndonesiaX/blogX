4b55a3728be236aa95161efbf8ab708b034b749f
hamzah <dehamzah@gmail.com>
2016-09-15 17:03:03 +0700

    add static base

---
+++ README.md
@@ -0,0 +1,3 @@
+#Jekyll blog of IndonesiaX Kabar. 
+
+Themes forked from [Long Haul](https://github.com/brianmaierjr/long-haul) by [@brianmaierjr](https://twitter.com/brianmaierjr).
---
+++ assets/js/scripts.js
@@ -0,0 +1,23 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+
+	// DropCap.js
+	var dropcaps = document.querySelectorAll(".dropcap");
+	window.Dropcap.layout(dropcaps, 2);
+
+	// Round Reading Time
+    $(".time").text(function (index, value) {
+      return Math.round(parseFloat(value));
+    });
+});
+
+// Mobile menu toggle
+(function($) {
+  var toggle_menu = $("#toggleMenu");
+  var menu_top = $("#menuTop");
+  toggle_menu.click(function() {
+    menu_top.toggleClass("active");
+  });
+})(jQuery);
+
+
