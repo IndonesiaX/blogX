d9ba88c9b68a604eccbb705e96505b2dd1110d0f
Dede Hamzah <dehamzah@gmail.com>
2017-03-17 16:57:18 +0700

    update static

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-sudo cp -R /usr/share/nginx/html ~/backup
-sudo cp -R * /usr/share/nginx/html
+# Backup first
+sudo cp -R /usr/share/nginx/html/* ~/backup/
+# Move the static to nginx root webserver
+sudo cp -R _site/* /usr/share/nginx/html/
+
+echo "The sites sucessfully updated"
